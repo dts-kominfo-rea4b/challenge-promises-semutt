1e8c77da102e188eb63182830810f09b437379d5
Semut <44838462+semutt@users.noreply.github.com>
2022-09-16 11:13:10 +0700

    melengkapi const promiseOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (mood) => {
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+
+  const concateJson = theaterIXX.concat(theaterVGC);
+  let totalMood = 0;
+
+  for (i = 0; i < concateJson.length; i++) {
+    if (concateJson[ i ].hasil == mood) {
+      totalMood += 1;
+    }
+  }
+
+  return totalMood;
+};
 
 module.exports = {
   promiseOutput,
